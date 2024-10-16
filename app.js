36fefad0d1d3942e8d43b6a411317a876a97fd26
khuzzzi <khuzema123kashif@gmail.com>
2024-10-16 22:43:52 +0100

    Initial commit

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ mongoose.connect(MONGO_URI,{
 app.use(express.json()); 
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: [],
     credentials: true
 }));
 app.use(cookieParser())
