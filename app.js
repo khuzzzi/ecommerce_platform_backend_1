6c6121b927d01691818fa483ac70e68450ee9d04
Khuzaima Kashif <83164491+khuzzzi@users.noreply.github.com>
2024-10-17 13:49:51 +0100

    Update app.js

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const MONGO_URI = process.env.MONGO_URI;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-    origin: "https://new-frontend-1-ecommerce-vercel-project.vercel.app/",
+    origin: "https://new-frontend-1-ecommerce.vercel.app", // Specify your frontend's origin
     credentials: true,
 }));
 app.use(cookieParser());
